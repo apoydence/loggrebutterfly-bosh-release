69680f3d133534792b2aa6216c76cd5caf88832f
Andrew Poydence <apoydence@gmail.com>
2017-02-14 06:27:28 -0700

    Add analyst job/package

--- scripts/blobify.sh
+++ scripts/blobify.sh
@@ -12,8 +12,13 @@ echo "building loggrebutterfly master"
 GOOS=linux go build -o $workspace/master github.com/apoydence/loggrebutterfly/master
 gzip -f $workspace/master
 
+echo "building loggrebutterfly analyst"
+GOOS=linux go build -o $workspace/analyst github.com/apoydence/loggrebutterfly/analyst
+gzip -f $workspace/analyst
+
 pushd $workspace > /dev/null
   bosh add-blob ./datanode.gz loggrebutterfly/datanode.gz > /dev/null
   bosh add-blob ./master.gz loggrebutterfly/master.gz > /dev/null
-  rm datanode.gz master.gz >> /dev/null
+  bosh add-blob ./analyst.gz loggrebutterfly/analyst.gz > /dev/null
+  rm datanode.gz master.gz analyst.gz >> /dev/null
 popd > /dev/null
